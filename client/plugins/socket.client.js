cca7a80f57018ef9f16d9da382ebc1a5c0b6998b
dima1501 <dima6343210@yandex.ru>
2021-09-27 15:40:22 +0300

    wip

--- client/plugins/socket.client.js
+++ client/plugins/socket.client.js
@@ -3,7 +3,7 @@ import VueSocketIO from 'vue-socket.io'
 // import store from '../store'
 import io from 'socket.io-client'
 
-const socketInstance = io(process.env.SERVER, {
+const socketInstance = io(process.env.server, {
   withCredentials: true,
   extraHeaders: {
     'Access-Control-Allow-Credentials': '*'

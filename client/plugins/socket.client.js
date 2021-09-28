d1bf018826ba041340fbc6ed2c6365f58cc47ca3
dima1501 <dima6343210@yandex.ru>
2021-09-28 14:33:19 +0300

    123

--- client/plugins/socket.client.js
+++ client/plugins/socket.client.js
@@ -13,7 +13,8 @@ const socketInstance = io(process.env.server, {
   reconnection: true,
   reconnectionDelay: 1000,
   reconnectionDelayMax: 5000,
-  reconnectionAttempts: 15
+  reconnectionAttempts: 15,
+  secure: true
 })
 
 export default ({ store }) => {

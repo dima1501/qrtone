77dfe9eb297721819ad67ab17c979608ed4c68d2
dima1501 <dima6343210@yandex.ru>
2021-09-29 13:55:31 +0300

    wqeq

--- server/src/app2.js
+++ server/src/app2.js
@@ -7,8 +7,8 @@ const path = require('path'),
       cors = require('cors'),
       fs = require('fs'),
       options = {
-        key: fs.readFileSync('./server.key'),
-        cert: fs.readFileSync('./certificate.crt')
+        key: fs.readFileSync('./key.pem'),
+        cert: fs.readFileSync('./cert.pem')
       },
       server = require("https").createServer(options, app),
       cluster = require('cluster'),

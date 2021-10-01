626e7b48ec503abc3c04991ad9af20212120e8b0
dima1501 <dima6343210@yandex.ru>
2021-10-01 23:08:27 +0300

    123

--- server/src/app2.js
+++ server/src/app2.js
@@ -46,7 +46,7 @@ if (cluster.isMaster) {
         key: fs.readFileSync('/etc/letsencrypt/live/toffee.menu/privkey.pem', 'utf8'),
         cert: fs.readFileSync('/etc/letsencrypt/live/toffee.menu/fullchain.pem', 'utf8')
      }, function(req, res) {
-         console.log(req.socket)
+         console.log(req.socket.connection())
 		var worker = workers[worker_index(req.socket.address().address, num_processes)];
 		worker.send('sticky-session:connection', req.socket);
 	});

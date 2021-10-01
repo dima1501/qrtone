18044ef841c353a8b79fea370afdcc561b5ceb19
dima1501 <dima6343210@yandex.ru>
2021-10-01 23:39:01 +0300

    123

--- server/src/app2.js
+++ server/src/app2.js
@@ -47,7 +47,7 @@ if (cluster.isMaster) {
         cert: fs.readFileSync('/etc/letsencrypt/live/toffee.menu/fullchain.pem', 'utf8')
      }, function(req, res) {
 		var worker = workers[worker_index(req.socket.address().address, num_processes)];
-		worker.send('sticky-session:connection', req);
+		worker.send('sticky-session:connection', {dima: '123', masha: '222'});
 	});
 
     server.listen(port, () => {

b71c23e0dd99f7ce2e7a92b2575d2c39e814dfa1
dima1501 <dima6343210@yandex.ru>
2021-10-05 17:26:51 +0300

    qwe

--- server/src/app4.js
+++ server/src/app4.js
@@ -1,14 +1,15 @@
 const cluster = require("cluster"),
     https = require("https"),
     numCPUs = require("os").cpus().length,
-    RedisStore = require("socket.io/lib/stores/redis"),
-    redis = require("socket.io/node_modules/redis"),
-    pub = redis.createClient(),
-    sub = redis.createClient(),
-    client = redis.createClient(),
+    // RedisStore = require("socket.io/lib/stores/redis"),
+    // redis = require("socket.io/node_modules/redis"),
+    // pub = redis.createClient(),
+    // sub = redis.createClient(),
+    // client = redis.createClient(),
     options = {
         key: fs.readFileSync('/etc/letsencrypt/live/toffee.menu/privkey.pem', 'utf8'),
-        cert: fs.readFileSync('/etc/letsencrypt/live/toffee.menu/fullchain.pem', 'utf8')};
+        cert: fs.readFileSync('/etc/letsencrypt/live/toffee.menu/fullchain.pem', 'utf8')},
+    sio_redis = require('socket.io-redis')
 
 if (cluster.isMaster) {
     let i = 0
@@ -32,11 +33,13 @@ if (cluster.isMaster) {
     let server = https.createServer(options, app)
     let io = require("socket.io").listen(server)
 
-    io.set("store", new RedisStore({
-        redisPub: pub,
-        redisSub: sub,
-        redisClient: client
-    }))
+    // io.set("store", new RedisStore({
+    //     redisPub: pub,
+    //     redisSub: sub,
+    //     redisClient: client
+    // }))
+
+    io.adapter(sio_redis({ host: 'localhost', port: 6379 }));
 
     server.listen(8000)
 

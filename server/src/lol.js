8bca843b53f88dd178fd49cf1d7fdd926a935f83
dima1501 <dima6343210@yandex.ru>
2021-10-16 10:14:23 +0300

    sa

--- server/src/lol.js
+++ server/src/lol.js
@@ -1,9 +1,9 @@
-// const cluster = require("cluster");
-// const https = require("http");
-// const { Server } = require("socket.io");
-// const numCPUs = require("os").cpus().length;
-// const { setupMaster, setupWorker } = require("@socket.io/sticky");
-// const { createAdapter, setupPrimary } = require("@socket.io/cluster-adapter");
+const cluster = require("cluster");
+const https = require("http");
+const { Server } = require("socket.io");
+const numCPUs = require("os").cpus().length;
+const { setupMaster, setupWorker } = require("@socket.io/sticky");
+const { createAdapter, setupPrimary } = require("@socket.io/cluster-adapter");
 
 const fs = require('fs'),
       options = {
@@ -64,11 +64,50 @@ const { setupWorker } = require("@socket.io/sticky");
 
 const httpServer = createServer(options);
 const io = require("socket.io")(httpServer, {
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  const httpServer = https.createServer(options);
+
+  // setup sticky sessions
+  setupMaster(httpServer, {
+    loadBalancingMethod: "least-connection",
+  });
+
+  // setup connections between the workers
+  setupPrimary();
+
+  // needed for packets containing buffers (you can ignore it if you only send plaintext objects)
+  // Node.js < 16.0.0
+  cluster.setupMaster({
+    serialization: "advanced",
+  });
+  // Node.js > 16.0.0
+  // cluster.setupPrimary({
+  //   serialization: "advanced",
+  // });
+
+  httpServer.listen(8000);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
+
+  const httpServer = https.createServer(options);
+  const io = new Server(httpServer, {
     cors: {
         origin: config.ORIGIN,
         credentials: true
     },
     transport: ['websocket']
+<<<<<<< HEAD
 })
 
 io.adapter(createAdapter());
@@ -78,3 +117,18 @@ setupWorker(io);
 io.on("connection", (socket) => {
   console.log(`connect ${socket.id}`);
 });
+=======
+  });
+
+  // use the cluster adapter
+  io.adapter(createAdapter());
+
+  // setup connection with the primary process
+  setupWorker(io);
+
+  io.on("connection", (socket) => {
+    /* ... */
+    console.log('Socket connected: ' + socket.id)
+  });
+}
+>>>>>>> parent of fa9fbb9 (qwe)

bafcb6186fc31ac2bec4f24ad32f5636d7f90145
dima1501 <dima6343210@yandex.ru>
2021-10-16 10:08:15 +0300

    123

--- client/plugins/socket.client.js
+++ client/plugins/socket.client.js
@@ -8,7 +8,7 @@ const socketInstance = io(process.env.server, {
   extraHeaders: {
     'Access-Control-Allow-Credentials': '*'
   },
-  transport: ['websocket'],
+  transport: ['websocket', 'polling'],
   rejectUnauthorized: false,
   reconnection: true,
   reconnectionDelay: 1000,
--- server/src/app3.js
+++ server/src/app3.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser')
 const routes = require('./routes')
 const cors = require('cors');
 const rateLimit = require("express-rate-limit")
-// const compression = require('compression')
 
 const { createServer } = require("https");
 const { Server } = require("socket.io");
@@ -63,7 +62,6 @@ app.use(cors({credentials: true, origin: config.ORIGIN}))
 app.use(apiLimiter)
 app.use(express.json({limit: '50mb'}))
 app.use(express.static('public'))
-// app.use(compression())
 app.use(cookieParser())
 app.use('/static', express.static(path.join(__dirname, '/static')))
 app.use('/', routes)

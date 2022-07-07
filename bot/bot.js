03089fa9de2b93b28fd55d5fa7c3a62e4ebd62ea
dima1501 <dima6343210@yandex.ru>
2022-07-07 22:41:51 +0300

    restart

--- bot/bot.js
+++ bot/bot.js
@@ -1,97 +1,138 @@
-require("dotenv").config({ path: "../.env" })
+require('dotenv').config({ path: '../.env' });
 
-const Telegraf = require('telegraf')
-const {Extra,  Markup, Stage, session } = Telegraf
-const { MongoClient, ObjectId } = require('mongodb')
+const Telegraf = require('telegraf');
+const { Extra, Markup, Stage, session } = Telegraf;
+const { MongoClient, ObjectId } = require('mongodb');
 
-const SceneGenerator = require('./Scenes')
+const SceneGenerator = require('./Scenes');
 
-const bot = new Telegraf(process.env.MODE == 'development' ? process.env.TELEGRAF_TOKEN : process.env.TELEGRAF_TOKEN_PROD , {polling: true})
-const api = require("../server/src/websocket")
+const bot = new Telegraf(
+  process.env.MODE == 'development'
+    ? process.env.TELEGRAF_TOKEN_PROD
+    : process.env.TELEGRAF_TOKEN_PROD,
+  { polling: true }
+);
+const api = require('../server/src/websocket');
 
-const curScene = new SceneGenerator()
+const curScene = new SceneGenerator();
 
-const loginScene = curScene.loginScene()
-const changeScene = curScene.changeScene()
-const logoutScene = curScene.logoutScene()
+const loginScene = curScene.loginScene();
+const changeScene = curScene.changeScene();
+const logoutScene = curScene.logoutScene();
 
-const stage = new Stage([loginScene, changeScene, logoutScene])
+const stage = new Stage([loginScene, changeScene, logoutScene]);
 
-bot.use(session())
-bot.use(stage.middleware())
+bot.use(session());
+bot.use(stage.middleware());
 
 MongoClient.connect(process.env.DB_URI, { useUnifiedTopology: true }).then(async (client) => {
-  const db = await client.db();
-  
-  bot.hears('/login', async ctx => {
-    ctx.scene.enter('login', { db: db })
-  })
+  const db = await client.db('dima');
 
-  bot.hears('/change', async ctx => {
-    ctx.scene.enter('change', { db: db })
-  })
+  bot.hears('/login', async (ctx) => {
+    ctx.scene.enter('login', { db: db });
+  });
 
-  bot.hears('/logout', async ctx => {
-    ctx.scene.enter('logout', { db: db })
-  })
+  bot.hears('/change', async (ctx) => {
+    ctx.scene.enter('change', { db: db });
+  });
+
+  bot.hears('/logout', async (ctx) => {
+    ctx.scene.enter('logout', { db: db });
+  });
 
   bot.action('like', async (ctx) => {
     try {
-      const tgUser = await db.collection('tgUsers').findOne({_id: ctx.update.callback_query.message.chat.id})
-      const user = await db.collection("users").aggregate([
-        { $match: { _id: ObjectId(tgUser.companyId) } },
-        { $unwind: '$notifications'},
-        { $match: {'notifications.chatId': {$in: [ctx.update.callback_query.message.chat.id]}}},
-        { $match: {'notifications.messageId': {$in: [ctx.update.callback_query.message.message_id]}}},
-        { $group: {_id: '$_id', list: {$push: '$notifications'}, sockets: {$push: '$sockets'}, id: {$push: '$_id'} } }
-      ]).toArray()
+      const tgUser = await db
+        .collection('tgUsers')
+        .findOne({ _id: ctx.update.callback_query.message.chat.id });
+      const user = await db
+        .collection('users')
+        .aggregate([
+          { $match: { _id: ObjectId(tgUser.companyId) } },
+          { $unwind: '$notifications' },
+          {
+            $match: {
+              'notifications.chatId': { $in: [ctx.update.callback_query.message.chat.id] },
+            },
+          },
+          {
+            $match: {
+              'notifications.messageId': { $in: [ctx.update.callback_query.message.message_id] },
+            },
+          },
+          {
+            $group: {
+              _id: '$_id',
+              list: { $push: '$notifications' },
+              sockets: { $push: '$sockets' },
+              id: { $push: '$_id' },
+            },
+          },
+        ])
+        .toArray();
 
       if (user[0]) {
         for (let i = 0; i < user[0].list[0].messages.length; i++) {
-          api.acceptFastActionTelegram({ sockets: user[0].sockets[0], data: { _id: user[0].list[0]._id } })
+          api.acceptFastActionTelegram({
+            sockets: user[0].sockets[0],
+            data: { _id: user[0].list[0]._id },
+          });
           ctx.telegram.editMessageText(
             user[0].list[0].messages[i].chat.id,
             user[0].list[0].messages[i].message_id,
             user[0].list[0].messages[i].message_id,
-            `${ctx.update.callback_query.message.text.replace('🙋🏼‍♂️', '✅').replace('⏳', '✅')}`,
+            `${ctx.update.callback_query.message.text.replace('🙋🏼‍♂️', '✅').replace('⏳', '✅')}`
           );
         }
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  })
+  });
 
   bot.action('accept', async (ctx) => {
     try {
-      const tgUser = await db.collection('tgUsers').findOne({_id: ctx.update.callback_query.message.chat.id})
-      const user = await db.collection("users").aggregate([
-        { $match: { _id: ObjectId(tgUser.companyId) } },
-        { $unwind: '$messages'},
-        { $match: {'messages.chatId': {$in: [ctx.update.callback_query.message.chat.id]}}},
-        { $match: {'messages.messageId': {$in: [ctx.update.callback_query.message.message_id]}}},
-        { $group: {_id: '$_id', list: {$push: '$messages'}, sockets: {$push: '$sockets'},  } }
-      ]).toArray()
+      const tgUser = await db
+        .collection('tgUsers')
+        .findOne({ _id: ctx.update.callback_query.message.chat.id });
+      const user = await db
+        .collection('users')
+        .aggregate([
+          { $match: { _id: ObjectId(tgUser.companyId) } },
+          { $unwind: '$messages' },
+          { $match: { 'messages.chatId': { $in: [ctx.update.callback_query.message.chat.id] } } },
+          {
+            $match: {
+              'messages.messageId': { $in: [ctx.update.callback_query.message.message_id] },
+            },
+          },
+          { $group: { _id: '$_id', list: { $push: '$messages' }, sockets: { $push: '$sockets' } } },
+        ])
+        .toArray();
 
       if (user[0]) {
         for (let i = 0; i < user[0].list[0].messages.length; i++) {
-          api.acceptOrderTelegram({ sockets: user[0].sockets, orderId: user[0].list[0].orderId, guestId: user[0].list[0].guestId })
+          api.acceptOrderTelegram({
+            sockets: user[0].sockets,
+            orderId: user[0].list[0].orderId,
+            guestId: user[0].list[0].guestId,
+          });
           ctx.telegram.editMessageText(
             user[0].list[0].messages[i].chat.id,
             user[0].list[0].messages[i].message_id,
             user[0].list[0].messages[i].message_id,
-            `${ctx.update.callback_query.message.text.replace('⏳', '✅').replace('🙋🏼‍♂️', '✅')}`,
+            `${ctx.update.callback_query.message.text.replace('⏳', '✅').replace('🙋🏼‍♂️', '✅')}`
           );
         }
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  })
+  });
 
-  process.once('SIGINT', () => bot.stop('SIGINT'))
-  process.once('SIGTERM', () => bot.stop('SIGTERM'))
-  bot.launch()
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+  bot.launch();
 });
 
-module.exports = bot.telegram
+module.exports = bot.telegram;

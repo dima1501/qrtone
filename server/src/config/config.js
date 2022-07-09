d04d8d9e2c92405e15829ddc8243d0827f376eff
dima1501 <dima6343210@yandex.ru>
2022-07-10 01:51:07 +0300

    wip

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -5,9 +5,9 @@ const config = {
   DB_NAME: process.env.DB_NAME,
   DB_URI: process.env.DB_URI,
   PORT: process.env.PORT || 3000,
-  ORIGIN: process.env.ORIGIN || 'https://toffee.menu:3000',
-  SERVER: process.env.SERVER || 'https://toffee.menu:8000',
-  API_SERVER: process.env.API_SERVER || 'https://toffee.menu:8000',
+  ORIGIN: process.env.ORIGIN || 'toffee.menu',
+  SERVER: process.env.SERVER || 'toffee.menu:8000',
+  API_SERVER: process.env.API_SERVER || 'toffee.menu:8000',
 };
 
 module.exports = config;

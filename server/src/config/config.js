c84dea9baba0823fac24688dd49b2715ffe16629
dima1501 <dima6343210@yandex.ru>
2022-07-10 01:12:38 +0300

    wip

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,13 +1,13 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') })
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
 const config = {
   DB_NAME: process.env.DB_NAME,
   DB_URI: process.env.DB_URI,
   PORT: process.env.PORT || 3000,
-  ORIGIN: process.env.ORIGIN || "http://localhost:3000",
-  SERVER: process.env.SERVER || "http://localhost:8000",
-  API_SERVER: process.env.API_SERVER || "http://localhost:8000"
-}
+  ORIGIN: process.env.ORIGIN || 'http://89.108.98.207:3000',
+  SERVER: process.env.SERVER || 'http://89.108.98.207:8000',
+  API_SERVER: process.env.API_SERVER || 'http://89.108.98.207:8000',
+};
 
-module.exports = config
+module.exports = config;

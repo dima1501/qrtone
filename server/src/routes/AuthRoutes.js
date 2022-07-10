720fddca5e87bd3ac9c702c2577d2fd2a981b232
dima1501 <dima6343210@yandex.ru>
2022-07-10 03:13:06 +0300

    wip

--- server/src/routes/AuthRoutes.js
+++ server/src/routes/AuthRoutes.js
@@ -1,105 +1,121 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const { ObjectId } = require('mongodb').ObjectID
+const { ObjectId } = require('mongodb').ObjectID;
 
-const auth = require('../middlewares/AuthMiddleware')
-const authGuest = require('../middlewares/AuthGuestMiddleware')
+const auth = require('../middlewares/AuthMiddleware');
+const authGuest = require('../middlewares/AuthGuestMiddleware');
 
-const UserService = require('../services/UserService')
-const SessionService = require('../services/SessionService')
+const UserService = require('../services/UserService');
+const SessionService = require('../services/SessionService');
 
-const crypto = require('crypto')
+const crypto = require('crypto');
 
-const PreloadUserModel = require('../models/PreloadUser')
+const PreloadUserModel = require('../models/PreloadUser');
 
-router.post("/api/checkauth", auth(), async (req, res) => {
+router.post('/api/checkauth', auth(), async (req, res) => {
   if (req.user) {
-    const preloadUser = await new PreloadUserModel(req.user)
-    res.send(preloadUser)
+    const preloadUser = await new PreloadUserModel(req.user);
+    res.send(preloadUser);
   } else {
-    res.send(false)
+    res.send(false);
   }
-})
+});
 
-router.post("/api/checkauth-guest", authGuest(), async (req, res) => {
-  res.send(req.user)
-})
+router.post('/api/checkauth-guest', authGuest(), async (req, res) => {
+  res.send(req.user);
+});
 
-router.post("/api/login", auth(), async (req, res) => {
-  const { email, password } = req.body
+router.post('/api/login', auth(), async (req, res) => {
+  const { email, password } = req.body;
   try {
-    const user = await new UserService().findUserByUsername(req, email)
-    const passwordHash = await crypto
-      .createHash("sha256")
-      .update(password)
-      .digest("hex")
+    const user = await new UserService().findUserByUsername(req, email);
+    const passwordHash = await crypto.createHash('sha256').update(password).digest('hex');
 
     if (!user || passwordHash !== user.password) {
-      res.status(200).send(false)
-      return
+      res.status(200).send(false);
+      return;
     }
-    
-    const preloadUser = await new PreloadUserModel(user)
-    const sessionId = await new SessionService().createSession(req, user._id)
-    res.cookie("sessionId", sessionId, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true }).send(preloadUser)
+
+    const preloadUser = await new PreloadUserModel(user);
+    const sessionId = await new SessionService().createSession(req, user._id);
+    res
+      .cookie('sessionId', sessionId, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: true,
+      })
+      .send(preloadUser);
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-})
+});
 
 router.post('/api/registration', auth(), async (req, res) => {
   if (!req.user) {
-      const findUser = await new UserService().findUserByUsername(req)
-      if (findUser) {
-          res.status(200).send(false)
+    const findUser = await new UserService().findUserByUsername(req);
+    if (findUser) {
+      res.status(200).send(false);
+    } else {
+      const createUser = await new UserService().createUser(req);
+      const preloadUser = await new PreloadUserModel(createUser);
+      if (createUser) {
+        const sessionId = await new SessionService().createSession(req, createUser._id);
+        console.log(sessionId);
+        res
+          .cookie('sessionId', sessionId, {
+            maxAge: 30 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            secure: true,
+          })
+          .send(preloadUser);
       } else {
-          const createUser = await new UserService().createUser(req)
-          const preloadUser = await new PreloadUserModel(createUser)
-          if (createUser) {
-              const sessionId = await new SessionService().createSession(req, createUser._id)
-              res.cookie("sessionId", sessionId, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true }).send(preloadUser)
-          } else {
-              res.status(200).send(false)
-          }
+        res.status(200).send(false);
       }
+    }
   } else {
-      res.status(200).send(false)
+    res.status(200).send(false);
   }
-})
+});
 
 router.post('/api/registration-guest', authGuest(), async (req, res) => {
   if (!req.user) {
-    const createUser = await new UserService().createGuest(req)
+    const createUser = await new UserService().createGuest(req);
     if (createUser) {
-        const sessionId = await new SessionService().createSession(req, createUser._id)
-        res.cookie("guestSessionId", sessionId, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true }).send(createUser)
+      const sessionId = await new SessionService().createSession(req, createUser._id);
+      res
+        .cookie('guestSessionId', sessionId, {
+          maxAge: 30 * 24 * 60 * 60 * 1000,
+          httpOnly: true,
+          secure: true,
+        })
+        .send(createUser);
     } else {
-        res.status(200).send(false)
+      res.status(200).send(false);
     }
   } else {
-      res.status(200).send(false)
+    res.status(200).send(false);
   }
-})
+});
 
-router.post("/api/logout", auth(), async (req, res) => {
-    if (!req.user) {
-      return res.redirect("/")
-    }
-  
-    try {
-      await new SessionService().deleteSession(req, req.sessionId)
-      res.clearCookie("sessionId").redirect("/")
-    } catch (err) {
-      console.error(err)
-    }
-})
+router.post('/api/logout', auth(), async (req, res) => {
+  if (!req.user) {
+    return res.redirect('/');
+  }
+
+  try {
+    await new SessionService().deleteSession(req, req.sessionId);
+    res.clearCookie('sessionId').redirect('/');
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 router.post('/api/set-socket-id', auth(), async (req, res) => {
   try {
     if (!req.user) {
-      res.send(false)
-      return
+      res.send(false);
+      return;
     }
     // await req.db.collection('users').updateOne(
     //   {
@@ -111,84 +127,89 @@ router.post('/api/set-socket-id', auth(), async (req, res) => {
     //     }
     //   }
     // )
-    res.send(true)
+    res.send(true);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
 
 router.post('/api/set-socket-id-guest', authGuest(), async (req, res) => {
   if (!req.user) {
-    return
+    return;
   }
   try {
     await req.db.collection('guests').updateOne(
       {
-        _id: ObjectId(req.user._id)
+        _id: ObjectId(req.user._id),
       },
       {
         $set: {
-          'socket': req.body.socketId
-        }
+          socket: req.body.socketId,
+        },
       }
-    )
-    res.send(true)
+    );
+    res.send(true);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
 
 router.delete('/api/delete-socket-id/:id', auth(), async (req, res) => {
   try {
-    const deleteSocketPlace = await req.db.collection('users').updateOne(
-      { 'sockets.socketId': req.params.id },
-      {'$pull': { "sockets": { "socketId": req.params.id } } }
-    )
-    const deleteSocketPublic = await req.db.collection('users').updateOne(
-      { 'publicSockets': { $in: [req.params.id] } },
-      { '$pull': { "publicSockets": req.params.id } }
-    )
+    const deleteSocketPlace = await req.db
+      .collection('users')
+      .updateOne(
+        { 'sockets.socketId': req.params.id },
+        { $pull: { sockets: { socketId: req.params.id } } }
+      );
+    const deleteSocketPublic = await req.db
+      .collection('users')
+      .updateOne(
+        { publicSockets: { $in: [req.params.id] } },
+        { $pull: { publicSockets: req.params.id } }
+      );
     if (deleteSocketPlace && deleteSocketPublic) {
-      res.send(true)
+      res.send(true);
     } else {
-      throw err()
+      throw err();
     }
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
 
-router.post("/api/check-key", async (req, res) => {
-  const user =  await req.db.collection("users").findOne({ restoreCode: +req.body.data.token })
+router.post('/api/check-key', async (req, res) => {
+  const user = await req.db.collection('users').findOne({ restoreCode: +req.body.data.token });
   if (user) {
-    res.status(200).send(true)
+    res.status(200).send(true);
   } else {
-    res.status(200).send(false)
+    res.status(200).send(false);
   }
-})
+});
 
-router.post("/api/update-password", async (req, res) => {
-  const user =  await req.db.collection("users").findOne({ restoreCode: +req.body.data.token })
+router.post('/api/update-password', async (req, res) => {
+  const user = await req.db.collection('users').findOne({ restoreCode: +req.body.data.token });
   if (user) {
     const passwordHash = await crypto
-        .createHash('sha256')
-        .update(req.body.data.newPassword)
-        .digest('hex')
+      .createHash('sha256')
+      .update(req.body.data.newPassword)
+      .digest('hex');
 
-    const setNewPassword = await req.db.collection("users").updateOne(
-      { restoreCode: +req.body.data.token, email: req.body.data.email },
-      { $set: { password: passwordHash } }
-    )
+    const setNewPassword = await req.db
+      .collection('users')
+      .updateOne(
+        { restoreCode: +req.body.data.token, email: req.body.data.email },
+        { $set: { password: passwordHash } }
+      );
 
     if (setNewPassword) {
-      res.send(true)
+      res.send(true);
     } else {
-      res.send(false)
+      res.send(false);
     }
-
   } else {
-    res.send(false)
+    res.send(false);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
